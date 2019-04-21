c7c803ebb939b072e6bb04b8599293a857b9c5ab
yanghanfei <yanghanfei@codemao.cn>
2019-04-21 12:57:29 +0800

    feat: add webpack

--- background.js
+++
@@ -1,7 +0,0 @@
-chrome.contextMenus.create({
-  title: "上传图片到七牛云",
-  contexts: ['image'],
-	onclick: function() {
-    
-  },
-});
---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const dist = path.resolve(__dirname, './dist');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    background: path.resolve(__dirname, './src/background'),
+    options: path.resolve(__dirname, './src/options'),
+    popup: path.resolve(__dirname, './src/popup'),
+  },
+  output: {
+    filename: 'js/[name].js',
+    path: dist,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader',
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: "[local]___[hash:base64:5]",
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: (loader) => [
+                require('postcss-preset-env')(),
+                require('autoprefixer')({
+                  'browsers': ['> 1%', 'last 2 versions'],
+                }),
+              ]
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js' ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'background.html',
+      template: './public/background.html',
+      chunks: ['background'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'options.html',
+      template: './public/options.html',
+      chunks: ['options'],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'popup.html',
+      template: './public/popup.html',
+      chunks: ['popup'],
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, './public/manifest.json'),
+        to: dist,
+      },
+      {
+        from: path.resolve(__dirname, './src/images'),
+        to: path.resolve(__dirname, './dist/images'),
+      }
+    ]),
+  ]
+}
