deede6c024309273e6acf4c73b68ab99b4b3b562
yanghanfei <yanghanfei@codemao.cn>
2019-04-23 16:12:30 +0800

    feat: add method handle url from context menu

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,30 @@
+import * as qiniu from './libs/qiniu';
+import { getBlob } from './libs/image';
+
 chrome.contextMenus.create({
   title: '上传图片到七牛云',
   contexts: ['image'],
   onclick: (info) => {
-    console.log(info.srcUrl);
+    handleImageUrlForContextMenu(info.srcUrl);
   },
 });
+
+async function handleImageUrlForContextMenu(url?:string) {
+  if (!url) { return; }
+
+  const blob = await getBlob(url);
+  const uploader = await qiniu.getUploader();
+
+  uploader(blob, 'image-from-background')
+    .subscribe({
+    next(res) {
+      console.log(res);
+    },
+    error(res) {
+      console.log(res);
+    },
+    complete(res) {
+      console.log(res);
+    },
+  });
+}
