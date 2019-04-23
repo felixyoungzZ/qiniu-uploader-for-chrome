69b0526feee6e632518c61aff7bf38cf667872b4
yanghanfei <yanghanfei@codemao.cn>
2019-04-23 16:13:33 +0800

    feat: add method handle file from uploadPage

--- src/uploadPage.ts
+++ src/uploadPage.ts
@@ -1,6 +1,7 @@
 import './styles/base.css';
 import './styles/uploadPage.css';
 
+import * as qiniu from './libs/qiniu';
 import { Dragger } from './libs/dragger';
 
 const dragArea = document.getElementById('drag-area') as HTMLElement;
@@ -11,6 +12,7 @@ const dragger = new Dragger(dragArea, {
 
 function dropCallback(e:DragEvent) {
   alert(e.dataTransfer!.files[0].name);
+  handleImageFileForUplaodPage(e.dataTransfer!.files[0]);
 }
 
 dragger.on();
@@ -19,4 +21,22 @@ const fileBox = document.getElementById('file-box') as HTMLInputElement;
 
 fileBox.addEventListener('change', function() {
   alert(this.files![0].name);
+  handleImageFileForUplaodPage(this.files![0]);
 });
+
+async function handleImageFileForUplaodPage(file:Blob) {
+  const uploader = await qiniu.getUploader();
+
+  uploader(file, 'image-from-upload-page')
+    .subscribe({
+    next(res) {
+      console.log(res);
+    },
+    error(res) {
+      console.log(res);
+    },
+    complete(res) {
+      console.log(res);
+    },
+  });
+}
