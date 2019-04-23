03fe07167ffeba16627828da5dae610deb48fff9
yanghanfei <yanghanfei@codemao.cn>
2019-04-23 16:14:06 +0800

    feat: init getUploader

--- src/libs/qiniu.ts
+++ src/libs/qiniu.ts
@@ -12,28 +12,61 @@ interface AuthOptions {
 }
 
 // Get upload token. From https://github.com/neal1991/image-host/blob/master/js/qiniu.js
+function generateUploadToken(options:AuthOptions) {
+  const {
+    accessKey,
+    secretKey,
+    bucket,
+  } = options;
 
-export const token = {
-  generate: function(options:AuthOptions) {
-    const {
-      accessKey,
-      secretKey,
-      bucket,
-    } = options;
+  const putPolicy = {
+    scope: bucket,
+    deadline: new Date().getTime() + MAX_AGE_OF_TOKEN,
+  };
 
-    const putPolicy = {
-      scope: bucket,
-      deadline: new Date().getTime() + MAX_AGE_OF_TOKEN,
-    };
+  const encoded = Base64.encode(UTF.utf16to8(JSON.stringify(putPolicy)));
+  const hash = CryptoJS.HmacSHA1(encoded, secretKey);
+  const encoded_signed = hash.toString(CryptoJS.enc.Base64);
+  return accessKey + ':' + Base64.safe(encoded_signed) + ':' + encoded;
+}
 
-    const encoded = Base64.encode(UTF.utf16to8(JSON.stringify(putPolicy)));
-    const hash = CryptoJS.HmacSHA1(encoded, secretKey);
-    const encoded_signed = hash.toString(CryptoJS.enc.Base64);
-    return accessKey + ':' + Base64.safe(encoded_signed) + ':' + encoded;
-  },
-  get: function() {},
-};
+interface QiniuConfig {
+  accessKey:string;
+  secretKey:string;
+  bucket:string;
+  domain:string;
+}
+async function getQiniuConfigFromChromeStorageSync() : Promise<QiniuConfig> {
+  return new Promise((resolve) => {
+    chrome.storage.sync.get((items) => {
+      if (
+        items['accessKey'] &&
+        items['secretKey'] &&
+        items['bucket'] &&
+        items['domain']
+      ) {
+        resolve({
+          accessKey: items['accessKey'],
+          secretKey: items['secretKey'],
+          bucket: items['bucket'],
+          domain: items['domain'],
+        });
+      }
+    });
+  });
+}
 
-export function upload(file:Blob) {
-  return qiniu.upload;
+export async function getUploader() {
+  const config = await getQiniuConfigFromChromeStorageSync();
+  const token = generateUploadToken({
+    accessKey: config.accessKey,
+    secretKey: config.secretKey,
+    bucket: config.bucket,
+  });
+
+  const uploadConfig = {
+    useCdnDomain: true,
+  };
+
+  return (file:Blob, uploadName:string) => qiniu.upload(file, uploadName, token, {}, uploadConfig);
 }
